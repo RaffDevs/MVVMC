684483557b00f1ddfb11effaf38ea25e02dc8f78
RaffDevs <rveronezdeveloper@gmail.com>
2023-04-08 12:16:12 -0300

    Initial Commit

---
+++ MVVMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMC
+//
+//  Created by Rafael Veronez Dias on 08/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
